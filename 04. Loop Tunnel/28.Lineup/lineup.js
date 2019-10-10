84abbe23b294c88337096ae35db25041810c1ac5
Vaqar <waqar3@gmail.com>
2019-10-10 21:43:25 +0100

    split function removal

--- 04. Loop Tunnel/28.Lineup/lineup.js
+++ 04. Loop Tunnel/28.Lineup/lineup.js
@@ -1,25 +1,23 @@
 function lineUp(commands) {
-    
-    const cmd = commands.split('');
-    let sameDirection = true; let count = 0;
-    
-    for(var i = 0; i < cmd.length; i++){
-        
-        if(cmd[i] == 'A' ){
-            if(sameDirection === true){
-            count += 1;
+
+    let sameDirection = true;
+    let count = 0;
+
+    for (var i = 0; i < commands.length; i++) {
+
+        if (commands[i] == 'A') {
+            if (sameDirection === true) {
+                count += 1;
             }
-        }
-        else {
-            if(sameDirection === false){
-            count += 1;
-            sameDirection = true;
-            }
-            else{
+        } else {
+            if (sameDirection === false) {
+                count += 1;
+                sameDirection = true;
+            } else {
                 sameDirection = false;
             }
         }
     }
-    
+
     return count;
 }
