1fb5433e73be047b460f2329bc2d91a5f153943f
Vaqar <waqar3@gmail.com>
2019-10-17 21:03:32 +0100

    Apple boxes Javascript solution

---
+++ 04. Loop Tunnel/30.Apple Boxes/apple_boxes.js
@@ -0,0 +1,11 @@
+function appleBoxes(k) {
+    
+    let yellow = 0; let red = 0;
+    
+    while(k){
+        (k&1)?(yellow += k*k):(red += k*k);
+        k--;
+    }
+    
+    return red-yellow;
+}
