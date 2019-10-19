c945ec905826b0c2ad2a2951b14305c7df0bf6e2
Vaqar <waqar3@gmail.com>
2019-10-19 22:50:35 +0100

    Candles Javascript code

---
+++ 04. Loop Tunnel/33.Candles/candles.js
@@ -0,0 +1,13 @@
+function candles(candlesNumber, makeNew) {
+    
+    let leftover = candlesNumber;
+    
+    while(leftover >= makeNew){
+        let nc = Math.floor(leftover/makeNew);
+        candlesNumber += nc;
+        leftover = nc + leftover%makeNew;   
+    }
+    
+    return candlesNumber;
+
+}
