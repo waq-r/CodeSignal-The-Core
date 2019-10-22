3e77e770285fb621546ad35a48af503c3950794f
Vaqar <waqar3@gmail.com>
2019-10-22 22:33:41 +0100

    Update count_black_cells.js

---
+++ 04. Loop Tunnel/34.Count Black Cells/count_black_cells.js
@@ -0,0 +1,13 @@
+function countBlackCells(n, m) {
+
+    function gcd(a, b) { 
+      if(b == 0){
+          return a;
+      }
+      else {
+          return gcd(b, a % b);
+      }
+    }
+    return n+m + gcd(n,m) -2;
+
+}
